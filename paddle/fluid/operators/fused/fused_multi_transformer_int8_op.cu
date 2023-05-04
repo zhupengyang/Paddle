30f93dcdb3d9b3af7c2ebcd42b565470c30403a5
zhengzekang <zhengzekang@baidu.com>
2023-05-04 16:30:36 +0800

    remove redundant modification

--- paddle/fluid/operators/fused/fused_multi_transformer_int8_op.cu
+++ paddle/fluid/operators/fused/fused_multi_transformer_int8_op.cu
@@ -356,8 +356,7 @@ class FusedMultiTransformerINT8OpKernel : public framework::OpKernel<T> {
                 num_head,
                 dim_head,
                 time_step->data<int>()[0],
-                1. / sqrt(dim_head), 
-                true);
+                1. / sqrt(dim_head));
       } else if (cache_kv_out) {  // generation context stage
         // TODO(wangxi): can remove dropout in inference
         fmha_compute.ComputeForward(qkv_out,
