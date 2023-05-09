64867a1ae4ca428802abf5908352dca43bdfe32b
lizhenyu04 <lizhenyu04@baidu.com>
2023-05-09 20:03:42 +0800

    make mp_ops supports bf16

--- python/paddle/distributed/fleet/layers/mpu/mp_ops.py
+++ python/paddle/distributed/fleet/layers/mpu/mp_ops.py
@@ -63,7 +63,7 @@ def _c_identity(tensor, group=None):
         check_variable_and_dtype(
             tensor,
             'tensor',
-            ['float16', 'float32', 'float64', 'int32', 'int64'],
+            ['uint16', 'float16', 'float32', 'float64', 'int32', 'int64'],
             '_c_identity',
         )
 
@@ -123,7 +123,7 @@ def _c_concat(tensor, group=None):
         check_variable_and_dtype(
             tensor,
             'tensor',
-            ['float16', 'float32', 'float64', 'int32', 'int64'],
+            ['uint16', 'float16', 'float32', 'float64', 'int32', 'int64'],
             '_c_concat',
         )
 
@@ -189,7 +189,7 @@ def _c_split(tensor, group=None):
         check_variable_and_dtype(
             tensor,
             'tensor',
-            ['float16', 'float32', 'float64', 'int32', 'int64'],
+            ['uint16', 'float16', 'float32', 'float64', 'int32', 'int64'],
             '_c_split',
         )
 
@@ -263,7 +263,7 @@ def _mp_allreduce(
         check_variable_and_dtype(
             tensor,
             'tensor',
-            ['float16', 'float32', 'float64', 'int32', 'int64'],
+            ['uint16', 'float16', 'float32', 'float64', 'int32', 'int64'],
             op_type,
         )
 
